1778d87316c6954c52cb1b6510214031cc7e73e9
Antonio Tarasco <tarascoant@gmail.com>
2019-05-01 19:43:32 +0200

    added selectmappanel

--- src/main/java/network/client/MainClient.java
+++ src/main/java/network/client/MainClient.java
@@ -9,12 +9,15 @@ import network.messages.serverToClient.BoardRequest;
 import network.messages.serverToClient.LoginResponse;
 import network.server.MainServer;
 import view.CLI.CLI;
+import view.GUI.GUI;
 import view.ViewInterface;
 
 import java.io.IOException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.UUID;
 import java.util.logging.Level;
@@ -33,40 +36,62 @@ public class MainClient {
 
     private static final Logger logger = Logger.getLogger(MainServer.class.getName());
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        System.out.println("RMI or SOCKET?[R/S]");
+        System.out.println("CLI or GUI?[C/G]");
         String choice = reader.nextLine().toLowerCase();
 
-        if(choice.equals("s")){
-            socket = true;
+        if (choice.equals("g")) {
+            MainClient mainClient = new MainClient();
+            view = new GUI(mainClient);
+
+            try {
+                view.launch();
+            } catch (Exception e) {
+                logger.log(Level.WARNING, "Unable to connect to server", e);
+            }
+            /*
+            //usato solo per test
+            Map<Integer,String> map = new HashMap<>();
+          map.put( 1 , "ideale per 3/4 giocatori");
+            map.put(  2,"ideale per 3/4 giocatori" );
+          map.put(  3,"third" );
+            map.put(  0,"quarto" );
+            view.chooseBoard(map);*/
         }
         else {
-            socket = false;
+
+
+            System.out.println("RMI or SOCKET?[R/S]");
+            choice = reader.nextLine().toLowerCase();
+
+            if (choice.equals("s")) {
+                socket = true;
+            } else {
+                socket = false;
+            }
+
+
+            System.out.println("localhost or remote?[L/R]");
+
+            choice = reader.nextLine().toLowerCase();
+            if (choice.equals("r")) {
+                System.out.println("Write IP address of the server:");
+                serverIp = reader.nextLine();
+            } else {
+                serverIp = "localhost";
+            }
+
+
+            MainClient mainClient = new MainClient();
+            view = new CLI(mainClient);
+            try {
+                view.launch();
+            } catch (Exception e) {
+                logger.log(Level.WARNING, "Unable to connect to server", e);
+            }
+
         }
-
-
-        System.out.println("localhost or remote?[L/R]");
-
-        choice = reader.nextLine().toLowerCase();
-        if(choice.equals("r")){
-            System.out.println("Write IP address of the server:");
-            serverIp = reader.nextLine();
-        }
-        else {
-            serverIp = "localhost";
-        }
-
-
-        MainClient mainClient = new MainClient();
-         view = new CLI(mainClient);
-        try {
-            view.launch();
-        }catch (Exception e ){
-            logger.log(Level.WARNING, "Unable to connect to server", e);
-        }
-
     }
 
     public void connect() throws  NotBoundException, IOException {
@@ -125,5 +150,7 @@ public class MainClient {
     public void setClientInterface(ClientInterface clientInterface){
         this.clientInterface = clientInterface;
     }
-
+    public void setSocket(Boolean connection){
+        this.socket=connection;
+    }
 }
